d75c777ff093d8771bbd2952950af2f838c1ac56
Jeff Hamilton <jham@android.com>
2010-02-05 17:50:26 -0600

    Remove unused static library.
    Change-Id: I7533dbca72f88322435196c55cb221341f31ac8b

--- Android.mk
+++ Android.mk
@@ -5,8 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := gsf-client \
-                               com.android.phone.common
+LOCAL_STATIC_JAVA_LIBRARIES := com.android.phone.common
 
 LOCAL_PACKAGE_NAME := Contacts
 LOCAL_CERTIFICATE := shared
