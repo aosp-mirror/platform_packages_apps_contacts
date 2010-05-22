d66b647fd85c488b90e15944c130788635ddedcd
Daisuke Miyakawa <dmiyakawa@google.com>
2010-05-21 17:04:26 -0700

    Modify ImportVCardService so that it does not rely on a stale constructor.
    We are not 100% sure whether this change is really correct though all tests are in green.
    
    Related change: I16e487fa
    
    Change-Id: I0a5aa1ea587c6b2d25ce90ba0a23bc14ea8a12f5

--- src/com/android/contacts/ImportVCardService.java
+++ src/com/android/contacts/ImportVCardService.java
@@ -255,7 +255,9 @@ public class ImportVCardService extends Service {
                 // at once. In the worst case, a user may call cancel() just before recreating
                 // mVCardParser.
                 synchronized (this) {
-                    mVCardParser = new VCardParser_V21(vcardType, charset);
+                    // TODO: ensure this change works fine.
+                    // mVCardParser = new VCardParser_V21(vcardType, charset);
+                    mVCardParser = new VCardParser_V21(vcardType);
                     if (mCanceled) {
                         mVCardParser.cancel();
                     }
@@ -275,7 +277,8 @@ public class ImportVCardService extends Service {
                     is = mResolver.openInputStream(uri);
 
                     synchronized (this) {
-                        mVCardParser = new VCardParser_V30(vcardType, charset);
+                        // mVCardParser = new VCardParser_V30(vcardType, charset);
+                        mVCardParser = new VCardParser_V30(vcardType);
                         if (mCanceled) {
                             mVCardParser.cancel();
                         }
