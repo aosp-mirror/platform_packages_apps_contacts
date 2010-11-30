f2da5b4e37e67b28e3bf4b32f4ae668ce0e74c3c
Dmitri Plotnikov <dplotnikov@google.com>
2010-11-29 17:58:47 -0800

    Using holo_light theme in date picker dialog
    Bug: 3229279
    Change-Id: If909f8b78a3dbc0e4bd215063f15bdb15a369d85

--- src/com/android/contacts/datepicker/DatePickerDialog.java
+++ src/com/android/contacts/datepicker/DatePickerDialog.java
@@ -104,7 +104,7 @@ public class DatePickerDialog extends AlertDialog implements OnClickListener,
             int dayOfMonth,
             boolean yearOptional) {
         this(context, context.getApplicationInfo().targetSdkVersion >= Build.VERSION_CODES.HONEYCOMB
-                        ? com.android.internal.R.style.Theme_Holo_Dialog_Alert
+                        ? com.android.internal.R.style.Theme_Holo_Light_Dialog_Alert
                         : com.android.internal.R.style.Theme_Dialog_Alert,
                 callBack, year, monthOfYear, dayOfMonth, yearOptional);
     }
