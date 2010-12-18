37dc7cf5eaf4e313a077e102ef86c422bc35df6e
Dmitri Plotnikov <dplotnikov@google.com>
2010-12-17 19:25:08 -0800

    Improving memory management in ContactPhotoLoader.
    What was happening was that during sync
    photos would get loaded and softly retained.
    The trouble was that GC would sometimes (often)
    free them before they got retained by the
    corresponding image view on the UI thread.
    
    Change-Id: I6fd05e2761384f5c0a7c9ba470b718237b9fbbf1

--- src/com/android/contacts/list/ContactPhotoLoader.java
+++ src/com/android/contacts/list/ContactPhotoLoader.java
@@ -82,6 +82,7 @@ public class ContactPhotoLoader implements Callback {
         private static final int LOADED_NEEDS_RELOAD = 3;
 
         int state;
+        Bitmap bitmap;
         SoftReference<Bitmap> bitmapRef;
     }
 
@@ -324,8 +325,20 @@ public class ContactPhotoLoader implements Callback {
             }
         }
 
+        softenCache();
+
         if (!mPendingRequests.isEmpty()) {
             requestLoading();
+        }
+    }
+
+    /**
+     * Removes strong references to loaded bitmaps to allow them to be garbage collected
+     * if needed.
+     */
+    private void softenCache() {
+        for (BitmapHolder holder : mBitmapCache.values()) {
+            holder.bitmap = null;
         }
     }
 
@@ -342,6 +355,7 @@ public class ContactPhotoLoader implements Callback {
         if (bytes != null) {
             try {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+                holder.bitmap = bitmap;
                 holder.bitmapRef = new SoftReference<Bitmap>(bitmap);
             } catch (OutOfMemoryError e) {
                 // Do nothing - the photo will appear to be missing
