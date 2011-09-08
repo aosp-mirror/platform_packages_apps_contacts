e5f8b369521d3b06fdb0c78a8f2f390e4fbb9004
Flavio Lerda <flerda@google.com>
2011-09-08 17:38:11 +0100

    Update icons for speaker phone.
    Use two different icons for speaker phone on and off states.
    
    Bug: 5048117
    Change-Id: I6fa9dad843a3aace2a693ab69acdae3b1d7277c7

--- src/com/android/contacts/voicemail/VoicemailPlaybackFragment.java
+++ src/com/android/contacts/voicemail/VoicemailPlaybackFragment.java
@@ -397,9 +397,10 @@ public class VoicemailPlaybackFragment extends Fragment {
         public void setSpeakerPhoneOn(boolean on) {
             getAudioManager().setSpeakerphoneOn(on);
             if (on) {
-                mPlaybackSpeakerphone.setImageResource(R.drawable.ic_sound_holo_dark);
+                mPlaybackSpeakerphone.setImageResource(R.drawable.ic_sound_speakerphone_holo_dark);
             } else {
-                mPlaybackSpeakerphone.setImageResource(R.drawable.ic_sound_holo_dark);
+                mPlaybackSpeakerphone.setImageResource(
+                        R.drawable.ic_sound_off_speakerphone_holo_dark);
             }
         }
 
