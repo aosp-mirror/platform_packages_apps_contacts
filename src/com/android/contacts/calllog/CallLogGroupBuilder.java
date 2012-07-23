e58bbc474eb60c403904b19259439fbf181a790e
Chiao Cheng <chiaocheng@google.com>
2012-07-23 13:56:04 -0700

    Dedup consecutive call log entries.
    - Aggregate missed calls along with incoming and outgoing calls.
    - Added SmallTest annotation to unit test.
    
    Change-Id: Icbcf0a113f49f099b6cb2ab5c3da2ab82aa9db4c

--- src/com/android/contacts/calllog/CallLogGroupBuilder.java
+++ src/com/android/contacts/calllog/CallLogGroupBuilder.java
@@ -76,13 +76,13 @@ public class CallLogGroupBuilder {
             } else if (!sameNumber) {
                 // Should only group with calls from the same number.
                 shouldGroup = false;
-            } else if (firstCallType == Calls.VOICEMAIL_TYPE
-                    || firstCallType == Calls.MISSED_TYPE) {
-                // Voicemail and missed calls should only be grouped with subsequent missed calls.
-                shouldGroup = callType == Calls.MISSED_TYPE;
+            } else if (firstCallType == Calls.VOICEMAIL_TYPE) {
+                // never group voicemail.
+                shouldGroup = false;
             } else {
-                // Incoming and outgoing calls group together.
-                shouldGroup = callType == Calls.INCOMING_TYPE || callType == Calls.OUTGOING_TYPE;
+                // Incoming, outgoing, and missed calls group together.
+                shouldGroup = (callType == Calls.INCOMING_TYPE || callType == Calls.OUTGOING_TYPE ||
+                        callType == Calls.MISSED_TYPE);
             }
 
             if (shouldGroup) {
