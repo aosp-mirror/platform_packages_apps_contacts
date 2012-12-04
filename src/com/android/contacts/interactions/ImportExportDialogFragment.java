e98d54ede8201d441125d4bbb2851952c4272d10
Chiao Cheng <chiaocheng@google.com>
2012-12-03 17:17:42 -0800

    Moving vcard UI to ContactsCommon.
    Moving all class in vcard directory in preparation to move
    ImportExportDialogFragment.
    
    Bug: 6993891
    Change-Id: I5a321892f86f788e874cd6ca0d29f3136645b495

--- src/com/android/contacts/interactions/ImportExportDialogFragment.java
+++ src/com/android/contacts/interactions/ImportExportDialogFragment.java
@@ -41,10 +41,10 @@ import com.android.contacts.R;
 import com.android.contacts.editor.SelectAccountDialogFragment;
 import com.android.contacts.common.model.AccountTypeManager;
 import com.android.contacts.common.model.account.AccountWithDataSet;
-import com.android.contacts.util.AccountSelectionUtil;
+import com.android.contacts.common.util.AccountSelectionUtil;
 import com.android.contacts.util.AccountsListAdapter.AccountListFilter;
-import com.android.contacts.vcard.ExportVCardActivity;
-import com.android.contacts.vcard.VCardCommonArguments;
+import com.android.contacts.common.vcard.ExportVCardActivity;
+import com.android.contacts.common.vcard.VCardCommonArguments;
 
 import java.util.List;
 
