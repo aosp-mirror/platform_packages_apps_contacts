9afe494fd2622d9dfae8a71ee4ebb9eda8c0baef
Jay Shrauner <shrauner@google.com>
2013-08-15 09:34:24 -0700

    Fix createCursorLoader NPE
    Pass through context saved from onAttach() to the Loader init rather
    than calling getActivity()
    
    Bug:10323136
    Change-Id: Ie7a23600361dd8681ee1b020b175fb36836be71a

--- src/com/android/contacts/common/list/ContactEntryListFragment.java
+++ src/com/android/contacts/common/list/ContactEntryListFragment.java
@@ -319,7 +319,7 @@ public abstract class ContactEntryListFragment<T extends ContactEntryListAdapter
                     ContactEntryListAdapter.LOCAL_INVISIBLE_DIRECTORY_ENABLED);
             return loader;
         } else {
-            CursorLoader loader = createCursorLoader();
+            CursorLoader loader = createCursorLoader(mContext);
             long directoryId = args != null && args.containsKey(DIRECTORY_ID_ARG_KEY)
                     ? args.getLong(DIRECTORY_ID_ARG_KEY)
                     : Directory.DEFAULT;
@@ -328,8 +328,8 @@ public abstract class ContactEntryListFragment<T extends ContactEntryListAdapter
         }
     }
 
-    public CursorLoader createCursorLoader() {
-        return new CursorLoader(mContext, null, null, null, null, null);
+    public CursorLoader createCursorLoader(Context context) {
+        return new CursorLoader(context, null, null, null, null, null);
     }
 
     private void startLoadingDirectoryPartition(int partitionIndex) {
