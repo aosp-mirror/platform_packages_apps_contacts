0af6f6014db33c091440663786c6a4184c53c252
Yorke Lee <yorkelee@google.com>
2014-05-20 10:49:04 -0700

    Add new AnimationUtils class in ContactsCommon
    Bug: 14900155
    Change-Id: I7ecdc5ae1a4287badcf5724eed5cf366496dee5a

---
+++ src/com/android/contacts/common/animation/AnimationUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.contacts.common.animation;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.view.View;
+
+public class AnimationUtils {
+    public static void crossFadeViews(final View fadeIn, final View fadeOut, int duration) {
+        fadeOut.animate().alpha(0).withLayer().setDuration(duration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        fadeOut.setVisibility(View.GONE);
+                    }
+                }).start();
+        fadeIn.setVisibility(View.VISIBLE);
+        fadeIn.animate().alpha(1).setDuration(duration).withLayer().setListener(null).start();
+    }
+}
