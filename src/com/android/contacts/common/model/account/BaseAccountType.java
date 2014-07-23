17f639c449f4b5dee93c1459d5731153a385f2d9
Paul Soulos <psoulos@google.com>
2014-07-23 13:33:25 -0700

    Fixes bug in Organization inflation
    Bug: 16491164
    Change-Id: Ie8f0a80fd9d6a6b44bc2ca1d0543965bbc6ca018

--- src/com/android/contacts/common/model/account/BaseAccountType.java
+++ src/com/android/contacts/common/model/account/BaseAccountType.java
@@ -369,8 +369,8 @@ public abstract class BaseAccountType extends AccountType {
     protected DataKind addDataKindOrganization(Context context) throws DefinitionException {
         DataKind kind = addKind(new DataKind(Organization.CONTENT_ITEM_TYPE,
                     R.string.organizationLabelsGroup, 5, true));
-        kind.actionHeader = new SimpleInflater(Organization.COMPANY);
-        kind.actionBody = new SimpleInflater(Organization.TITLE);
+        kind.actionHeader = new SimpleInflater(R.string.organizationLabelsGroup);
+        kind.actionBody = ORGANIZATION_BODY_INFLATER;
         kind.typeOverallMax = 1;
 
         kind.fieldList = Lists.newArrayList();
@@ -630,6 +630,24 @@ public abstract class BaseAccountType extends AccountType {
             }
         }
     }
+
+    public static final StringInflater ORGANIZATION_BODY_INFLATER = new StringInflater() {
+        @Override
+        public CharSequence inflateUsing(Context context, ContentValues values) {
+            final CharSequence companyValue = values.containsKey(Organization.COMPANY) ?
+                    values.getAsString(Organization.COMPANY) : null;
+            final CharSequence titleValue = values.containsKey(Organization.TITLE) ?
+                    values.getAsString(Organization.TITLE) : null;
+
+            if (companyValue != null && titleValue != null) {
+                return companyValue +  ": " + titleValue;
+            } else if (companyValue == null) {
+                return titleValue;
+            } else {
+                return companyValue;
+            }
+        }
+    };
 
     @Override
     public boolean isGroupMembershipEditable() {
@@ -1229,8 +1247,8 @@ public abstract class BaseAccountType extends AccountType {
             final DataKind kind = newDataKind(context, parser, attrs, false,
                     Organization.CONTENT_ITEM_TYPE, null, R.string.organizationLabelsGroup,
                     Weight.ORGANIZATION,
-                    new SimpleInflater(Organization.COMPANY),
-                    new SimpleInflater(Organization.TITLE));
+                    new SimpleInflater(R.string.organizationLabelsGroup),
+                    ORGANIZATION_BODY_INFLATER);
 
             kind.fieldList.add(new EditField(Organization.COMPANY, R.string.ghostData_company,
                     FLAGS_GENERIC_NAME));
