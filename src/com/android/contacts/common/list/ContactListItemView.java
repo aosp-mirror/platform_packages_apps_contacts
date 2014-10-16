1a21fe5123f737bee9f47b27de944825aba9c459
Walter Jang <wjang@google.com>
2014-10-15 18:32:44 -0700

    Set TtsSpans on names and snippets that could be phone numbers
    Bug: 17322140
    
    Change-Id: I9afa9c59e1db6c5ad7123bdb37d288fa64faeddf

--- src/com/android/contacts/common/list/ContactListItemView.java
+++ src/com/android/contacts/common/list/ContactListItemView.java
@@ -49,6 +49,7 @@ import com.android.contacts.common.ContactPresenceIconUtil;
 import com.android.contacts.common.ContactStatusUtil;
 import com.android.contacts.common.R;
 import com.android.contacts.common.format.TextHighlighter;
+import com.android.contacts.common.util.ContactDisplayUtils;
 import com.android.contacts.common.util.SearchUtil;
 import com.android.contacts.common.util.ViewUtil;
 
@@ -1038,6 +1039,13 @@ public class ContactListItemView extends ViewGroup
         } else {
             mTextHighlighter.setPrefixText(getSnippetView(), text, mHighlightedPrefix);
             mSnippetView.setVisibility(VISIBLE);
+            if (ContactDisplayUtils.isPossiblePhoneNumber(text)) {
+                // Give the text-to-speech engine a hint that it's a phone number
+                mSnippetView.setContentDescription(
+                        ContactDisplayUtils.getTelephoneTtsSpannable(text));
+            } else {
+                mSnippetView.setContentDescription(null);
+            }
         }
     }
 
@@ -1149,6 +1157,14 @@ public class ContactListItemView extends ViewGroup
             name = mUnknownNameText;
         }
         setMarqueeText(getNameTextView(), name);
+
+        if (ContactDisplayUtils.isPossiblePhoneNumber(name)) {
+            // Give the text-to-speech engine a hint that it's a phone number
+            mNameTextView.setContentDescription(
+                    ContactDisplayUtils.getTelephoneTtsSpannable(name.toString()));
+        } else {
+            mNameTextView.setContentDescription(null);
+        }
     }
 
     public void hideDisplayName() {
