07adabf28fc1b844c81d5635a87751c20f0bd1aa
Jay Shrauner <shrauner@google.com>
2015-04-09 11:23:13 -0700

    Build Contacts against current SDK
    Bug:
    Change-Id: I31222599eb1da74167c69fdbbaca5590ca603b8a

--- Android.mk
+++ Android.mk
@@ -36,6 +36,8 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_SDK_VERSION := current
+
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
