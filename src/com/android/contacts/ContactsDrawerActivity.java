92d68785b8a2e55fcd854f6196f9e9b8e2897cd3
Walter Jang <wjang@google.com>
2016-07-09 15:31:24 -0700

    Finish current group activity after creating new groups
    Bug 30047708
    
    Change-Id: I70aca1320dfb8a44d1a140d154a6f172ab438315

--- src/com/android/contacts/ContactsDrawerActivity.java
+++ src/com/android/contacts/ContactsDrawerActivity.java
@@ -245,6 +245,13 @@ public abstract class ContactsDrawerActivity extends AppCompatContactsActivity i
             if (Log.isLoggable(TAG, Log.VERBOSE)) Log.v(TAG, "Received group URI " + groupUri);
             Toast.makeText(this, R.string.groupCreatedToast, Toast.LENGTH_SHORT).show();
             startActivity(GroupUtil.createViewGroupIntent(this, groupUri, /* title */ null));
+            if (this instanceof GroupMembersActivity) {
+                // If we created a group while viewing the members of an existing group (i.e.
+                // while on GroupMembersActivity), finish the current GroupMembersActivity so that
+                // hitting back from the new GroupMembersActivity that was just stared will open
+                // the all contacts list. See b/30047708.
+                finish();
+            }
         } else {
             super.onNewIntent(newIntent);
         }
