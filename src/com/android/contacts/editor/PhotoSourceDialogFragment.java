f34bd4235c0ca3f1cf8b192bb572a4e463bc36ff
guanxiongliu <guanxiongliu@google.com>
2016-07-29 20:24:00 -0700

    Bringing import/export dialog and edit photo dialog up to spec (1/2)
    Bug:30079784
    Change-Id: I7e05b5ec089ad7b612d19afe349ce52ea3b3334a

--- src/com/android/contacts/editor/PhotoSourceDialogFragment.java
+++ src/com/android/contacts/editor/PhotoSourceDialogFragment.java
@@ -26,6 +26,8 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -91,8 +93,10 @@ public class PhotoSourceDialogFragment extends DialogFragment {
         };
 
         // Build the AlertDialog
+        final TextView title = (TextView) View.inflate(getActivity(), R.layout.dialog_title, null);
+        title.setText(R.string.menu_change_photo);
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.menu_change_photo);
+        builder.setCustomTitle(title);
         builder.setItems(items, clickListener);
         builder.setNegativeButton(android.R.string.cancel, /* listener =*/ null);
         return builder.create();
