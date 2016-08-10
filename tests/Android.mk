a6fb6d2b6f0461871797f11faf72eb19b7f55373
Walter Jang <wjang@google.com>
2016-08-10 09:37:52 -0700

    Fix build after copy of ContactsCommon dirs to Contacts
    * Remove all references to the GoogleContactsCommon and
      ContactsCommon dirs
    * Rename the aosp_contacts_dir variable to just contacts_dir
    * Move contactsbind and commonbind to src-bind so that they
      can be excluded from the GoogleContacts build
    
    Bug 30759296
    
    Change-Id: I1a1efd20bce41049e311283fb7b4d9f214b24863

--- tests/Android.mk
+++ tests/Android.mk
@@ -7,11 +7,11 @@ LOCAL_CERTIFICATE := shared
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-test
 
-src_dirs := src \
-    ../../ContactsCommon/TestCommon/src
+src_dirs := src ../TestCommon/src
+res_dirs := res res-common
 
-# Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 
 LOCAL_PACKAGE_NAME := ContactsTests
 
@@ -19,5 +19,12 @@ LOCAL_INSTRUMENTATION_FOR := Contacts
 
 LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 21
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    mockito-target
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages com.android.contacts.common.tests
 
 include $(BUILD_PACKAGE)
